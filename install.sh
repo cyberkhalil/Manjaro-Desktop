502ae16843b4e98a58219d44854e3529beaf9833
Mahmoud khalil <khalil2535@protonmail.com>
2019-09-22 18:21:07 +0300

    split yay command

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 sudo pacman -Syu --noconfirm
 sudo pacman -S octopi yay virtualbox flameshot korganizer kmail kontact brave vlc simplescreenrecorder --noconfirm
-yay kazam ttf-vista-fonts ttf-ms-fonts
+yay kazam 
+yay ttf-vista-fonts 
+yay ttf-ms-fonts
 mkdir -p ~/.config/fontconfig/conf.d/
 mv ./.config/fontconfig/conf.d/93-ar.conf ~/.config/fontconfig/conf.d/93-ar.conf
