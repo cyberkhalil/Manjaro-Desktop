c2e909c465b0136904531b68981bbbf07fded95c
Mahmoud khalil <khalil2535@protonmail.com>
2019-09-22 18:03:54 +0300

    fix mkdir

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 sudo pacman -Syu --noconfirm
 sudo pacman -S octopi yay virtualbox flameshot korganizer kmail kontact brave vlc simplescreenrecorder --noconfirm
 yay kazam ttf-vista-fonts ttf-ms-fonts --noconfirm
-mkdirs ~/.config/fontconfig/conf.d/
+mkdir -p ~/.config/fontconfig/conf.d/
 mv ./.config/fontconfig/conf.d/93-ar.conf ~/.config/fontconfig/conf.d/93-ar.conf
