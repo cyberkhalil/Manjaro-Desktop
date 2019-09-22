5c36f28fcec05fe0c1dd0fb5033095e858da2b32
Mahmoud khalil <khalil2535@protonmail.com>
2019-09-22 20:07:19 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 sudo pacman -Syu --noconfirm
 sudo pacman -S octopi yay virtualbox flameshot korganizer kmail kontact brave vlc simplescreenrecorder --noconfirm
-yay kazam 
-yay ttf-vista-fonts 
-yay ttf-ms-fonts
+yay -S kazam ttf-vista-fonts ttf-ms-fonts
 mkdir -p ~/.config/fontconfig/conf.d/
 mv ./.config/fontconfig/conf.d/93-ar.conf ~/.config/fontconfig/conf.d/93-ar.conf
