8babcbad5323d28069909773bbf2783afffc5c72
Mahmoud khalil <khalil2535@protonmail.com>
2019-09-22 17:51:28 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,4 @@
+sudo pacman -Syu
+sudo pacman -S octopi yay virtualbox flameshot korganizer kmail kontact brave vlc simplescreenrecorder --noconfirm
+yay kazam ttf-vista-fonts ttf-ms-fonts
+mv ./.config/fontconfig/conf.d/93-ar.conf ~/.config/fontconfig/conf.d/93-ar.conf
