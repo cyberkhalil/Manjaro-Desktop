227818e923508f6eb1cc7748ef28daf5a58f89f5
Mahmoud khalil <khalil2535@protonmail.com>
2019-09-22 17:59:38 +0300

    add no confirm for pacman update

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-sudo pacman -Syu
+sudo pacman -Syu --noconfirm
 sudo pacman -S octopi yay virtualbox flameshot korganizer kmail kontact brave vlc simplescreenrecorder --noconfirm
 yay kazam ttf-vista-fonts ttf-ms-fonts --noconfirm
 mv ./.config/fontconfig/conf.d/93-ar.conf ~/.config/fontconfig/conf.d/93-ar.conf
