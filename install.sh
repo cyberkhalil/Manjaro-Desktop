a0a4a55a3cd28eec5a7ecbe3f01d07df9a72aad5
Mahmoud khalil <khalil2535@protonmail.com>
2020-02-27 20:02:05 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 sudo pacman -Syu --noconfirm
-sudo pacman -S octopi yay virtualbox flameshot korganizer kmail kontact brave vlc simplescreenrecorder linux52-virtualbox-host-modules --noconfirm
+sudo pacman -S yay virtualbox flameshot korganizer kmail kontact brave vlc simplescreenrecorder --noconfirm
 yay -S kazam ttf-vista-fonts ttf-ms-fonts
+
+# to solve arabic language font problem
 mkdir -p ~/.config/fontconfig/conf.d/
 mv ./.config/fontconfig/conf.d/93-ar.conf ~/.config/fontconfig/conf.d/93-ar.conf
