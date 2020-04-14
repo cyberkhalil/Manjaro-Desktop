f2ca419e85891e322c9bb09e0143a2b91d3908c6
Mahmoud khalil <khalil2535@protonmail.com>
2020-04-14 21:57:47 +0300

    no need in 19

--- install.sh
+++ install.sh
@@ -1,7 +1,3 @@
 sudo pacman -Syu --noconfirm
 sudo pacman -S yay virtualbox flameshot korganizer kmail kontact brave vlc simplescreenrecorder linux linux-headers --noconfirm
 yay -S ttf-vista-fonts ttf-ms-fonts
-
-# to solve arabic language font problem
-mkdir -p ~/.config/fontconfig/conf.d/
-mv ./.config/fontconfig/conf.d/93-ar.conf ~/.config/fontconfig/conf.d/93-ar.conf
