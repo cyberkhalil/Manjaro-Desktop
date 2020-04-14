5de0c5c391a33f7f17bbc74c23fccc9d745bca2a
Mahmoud khalil <khalil2535@protonmail.com>
2020-04-14 23:30:43 +0300

    add akregator

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 sudo pacman -Syu --noconfirm
-sudo pacman -S yay virtualbox flameshot korganizer kmail kontact brave vlc simplescreenrecorder linux linux-headers --noconfirm
+sudo pacman -S yay virtualbox flameshot korganizer akregator kmail kontact brave vlc simplescreenrecorder linux linux-headers --noconfirm
 yay -S ttf-vista-fonts ttf-ms-fonts
