9f01798ab4c10b796d43b30cb67d21bc92f10241
Mahmoud khalil <khalil2535@protonmail.com>
2020-07-31 21:16:35 +0300

    Update install.sh For 20.03

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 sudo pacman -Syu --noconfirm
-sudo pacman -S yay virtualbox flameshot korganizer akregator kmail kontact brave vlc simplescreenrecorder linux linux-headers --noconfirm
-yay -S ttf-vista-fonts ttf-ms-fonts
+sudo pacman -S yay virtualbox flameshot korganizer akregator kmail kontact brave vlc simplescreenrecorder linux linux-headers bash-completion --noconfirm
+yay -R pamac-gtk pamac-tray-appindicator pamac-snap-plugin pamac-flatpak-plugin --noconfirm
+yay -S octopi ttf-vista-fonts ttf-ms-fonts --noconfirm
