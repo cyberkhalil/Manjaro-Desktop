098f39d1a5cf0f9d6104ac0cee7fbf1fa27fa788
Mahmoud khalil <khalil2535@protonmail.com>
2020-11-25 22:06:36 +0200

    update for 20.1.2

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 sudo pacman -Syu --noconfirm
-sudo pacman -S yay virtualbox flameshot korganizer akregator kmail kontact brave vlc simplescreenrecorder linux linux-headers bash-completion --noconfirm
+sudo pacman -S yay virtualbox flameshot korganizer akregator kmail kontact brave vlc simplescreenrecorder linux linux-headers bash-completion binutils --noconfirm
 yay -R pamac-gtk pamac-tray-icon-plasma pamac-snap-plugin pamac-flatpak-plugin web-installer-url-handler manjaro-hello web-installer-url-handler --noconfirm
 yay -S octopi ttf-vista-fonts ttf-ms-fonts --noconfirm
