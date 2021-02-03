5c27b1b338347f0b64936a452d545d27fd89239c
Mahmoud khalil <khalil2535@protonmail.com>
2021-02-03 22:34:11 +0200

    add virtualbox-host-dkms

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 sudo pacman -Syu --noconfirm
-sudo pacman -S yay virtualbox flameshot korganizer akregator kmail kontact brave vlc obs-studio linux linux-headers bash-completion base-devel --noconfirm
+sudo pacman -S yay virtualbox flameshot korganizer akregator kmail kontact brave vlc obs-studio linux linux-headers bash-completion base-devel virtualbox-host-dkms --noconfirm
 yay -R pamac-gtk pamac-tray-icon-plasma pamac-snap-plugin pamac-flatpak-plugin web-installer-url-handler manjaro-hello web-installer-url-handler yakuake --noconfirm
 yay -S octopi ttf-vista-fonts ttf-ms-fonts --noconfirm
