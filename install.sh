889ed7e1495564e5e6dc1c08127e97825f4e28b6
Mahmoud khalil <khalil2535@protonmail.com>
2021-02-05 22:10:55 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,62 @@
+# update repos
 sudo pacman -Syu --noconfirm
-sudo pacman -S yay virtualbox flameshot korganizer akregator kmail kontact brave vlc obs-studio linux linux-headers bash-completion base-devel virtualbox-host-dkms --noconfirm
+
+# install yay CLI package manager
+sudo pacman -S yay --noconfirm
+
+# Remove default package manager
 yay -R pamac-gtk pamac-tray-icon-plasma pamac-snap-plugin pamac-flatpak-plugin web-installer-url-handler manjaro-hello web-installer-url-handler yakuake --noconfirm
-yay -S octopi ttf-vista-fonts ttf-ms-fonts --noconfirm
+
+## Installation
+# Compilers & utilities
+yay -S bash-completion base-devel --noconfirm
+# Kernals
+yay -S linux linux-headers --noconfirm
+
+# Virtualization
+yay -S virtualbox virtualbox-host-dkms --noconfirm
+
+# Screenshot & Video recording
+yay -S flameshot obs-studio --noconfirm
+
+# Video & Audio players
+yay -S vlc --noconfirm
+
+# Video Editor
+yay -S kdenlive --noconfirm
+
+# Browsers
+yay -S brave --noconfirm
+
+# GUI package manager
+yay -S octopi --noconfirm # bauh
+
+# Fonts
+yay -S ttf-vista-fonts ttf-ms-fonts --noconfirm
+
+# Code Editors
+yay -S visual-studio-code-bin --noconfirm # atom
+
+# Network Tools
+yay -S wireshark-qt aircrack-ng zenmap --noconfirm
+
+# Manage Tasks
+yay -S korganizer gnome-todo --noconfirm
+
+# Rss Feeds
+yay -S akregator --noconfirm
+
+# Mail Clients
+yay -S kmail --noconfirm
+
+# Contacts
+yay -S kontact --noconfirm
+
+# Harddisk manager
+yay -S gparted --noconfirm
+
+# Chat
+yay -S telegram-desktop --noconfirm
+
+# Remove default dependencies
+yay -c --noconfirm
